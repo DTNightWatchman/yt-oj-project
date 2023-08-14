3dbee58ddc9b829f8197fca864e863e0f64717dd
DT守夜人 <ytbaiduren@163.com>
2023-08-14 22:37:05 +0800

    引入md编辑器组件

---
+++ ytoj-frontend/src/components/MdEditor.vue
@@ -0,0 +1,34 @@
+<template>
+  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import { Editor, Viewer } from "@bytemd/vue-next";
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import { ref, withDefaults, defineProps } from "vue";
+
+const plugins = [
+  gfm(),
+  highlight(),
+  // Add more plugins here
+];
+
+interface Props {
+  value: string;
+  handleChange: (v: string) => void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+</script>
+
+<style scoped>
+.bytemd-toolbar-icon.bytemd-tippy.bytemd-tippy-right:last-child {
+  display: none;
+}
+</style>
