6b296764f0685f25d92943e2dfe0357eab7f50c8
DT守夜人 <ytbaiduren@163.com>
2023-08-17 21:39:14 +0800

    代码编辑组件

---
+++ ytoj-frontend/src/components/CodeEditor.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="code-editor" ref="codeEditorRef" style="min-height: 400px"></div>
+</template>
+
+<script setup lang="ts">
+import * as monaco from "monaco-editor";
+import { onMounted, ref, toRaw } from "vue";
+import { defineProps, withDefaults } from "vue/dist/vue";
+
+interface Props {
+  value: string;
+  handleChange: (v: string) => void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+
+const codeEditorRef = ref();
+const codeEditor = ref();
+
+onMounted(() => {
+  if (!codeEditorRef.value) {
+    return;
+  }
+  // Hover on each property to see its docs!
+  codeEditor.value = monaco.editor.create(codeEditorRef.value, {
+    value: props.value,
+    language: "java",
+    automaticLayout: true,
+    minimap: {
+      enabled: true,
+    },
+    // lineNumbers: "off",
+    // roundedSelection: false,
+    // scrollBeyondLastLine: false,
+    readOnly: false,
+    theme: "vs-dark",
+  });
+
+  codeEditor.value.onDidChangeModelContent(() => {
+    props.handleChange(toRaw(codeEditor.value).getValue());
+  });
+});
+</script>
+
+<style scoped></style>
